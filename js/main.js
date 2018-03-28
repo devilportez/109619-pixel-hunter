d0f50924b7830d55c91c3f5f03d003057f362e0a
Stas Melekhov (devilportez) <devilportez@icloud.com>
2018-03-28 17:22:10 +0500

    Improvements

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+const RIGHT_ARROW_KEY = 39;
+const LEFT_ARROW_KEY = 37;
+
 const templates = document.querySelectorAll(`template`);
 const mainContainer = document.querySelector(`.central`);
 let counter = 0;
@@ -12,18 +15,15 @@ const showScreen = (number) => {
 };
 
 const onKeyDown = (event) => {
-  const RIGHT_ARROW_KEY = 39;
-  const LEFT_ARROW_KEY = 37;
+  const isRightArrow = event.keyCode === RIGHT_ARROW_KEY;
+  const isLeftArrow = event.keyCode === LEFT_ARROW_KEY;
+  const isCounterTopLimit = counter < templates.length - 1;
 
-  const rightArrow = event.keyCode === RIGHT_ARROW_KEY;
-  const leftArrow = event.keyCode === LEFT_ARROW_KEY;
-  const counterTopLimiter = counter < templates.length - 1;
-
-  if (event.altKey && rightArrow && counterTopLimiter) {
+  if (event.altKey && isRightArrow && isCounterTopLimit) {
     counter++;
     showScreen(counter);
   }
-  if (event.altKey && leftArrow && counter > 0) {
+  if (event.altKey && isLeftArrow && counter > 0) {
     counter--;
     showScreen(counter);
   }
