b6a8a218a47f8b3ee7af6c9bc545b33593daaad4
Stas Melekhov (devilportez) <devilportez@icloud.com>
2018-03-28 15:11:30 +0500

    Improvements

--- js/main.js
+++ js/main.js
@@ -5,20 +5,25 @@ let counter = 0;
 const showScreen = (number) => {
   const fragment = document.createDocumentFragment();
 
-  while (mainContainer.lastChild) {
-    mainContainer.removeChild(mainContainer.lastChild);
-  }
+  mainContainer.innerHTML = ``;
 
   fragment.appendChild(templates[number].content.cloneNode(true));
   mainContainer.appendChild(fragment);
 };
 
 const onKeyDown = (event) => {
-  if (event.altKey && event.keyCode === 39 && counter < templates.length - 1) {
+  const RIGHT_ARROW_KEY = 39;
+  const LEFT_ARROW_KEY = 37;
+
+  const rightArrow = event.keyCode === RIGHT_ARROW_KEY;
+  const leftArrow = event.keyCode === LEFT_ARROW_KEY;
+  const counterTopLimiter = counter < templates.length - 1;
+
+  if (event.altKey && rightArrow && counterTopLimiter) {
     counter++;
     showScreen(counter);
   }
-  if (event.altKey && event.keyCode === 37 && counter > 0) {
+  if (event.altKey && leftArrow && counter > 0) {
     counter--;
     showScreen(counter);
   }
