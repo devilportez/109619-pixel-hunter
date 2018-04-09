328f3625cfa5d38324a89e0674db8034af3d5643
Stas Melekhov (devilportez) <devilportez@icloud.com>
2018-04-09 21:17:23 +0500

    Changes

--- js/countScore.js
+++ js/countScore.js
@@ -1,21 +1,25 @@
 const countScore = (answers, lives) => {
+  const CORRECT_ANSWER = 100;
+  const CORRECT_FAST_ANSWER = 150;
+  const CORRECT_SLOW_ANSWER = 50;
+  const LIVE_COST = 50;
   let score = 0;
 
   if (answers.length < 10) {
     return -1;
   }
   answers.map((answer) => {
-    if (answer.isCorrect) {
-      score += 100;
+    if (answer.isCorrect && answer.time >= 10 && answer.time <= 20) {
+      score += CORRECT_ANSWER;
     }
     if (answer.isCorrect && answer.time < 10) {
-      score += 50;
+      score += CORRECT_FAST_ANSWER;
     }
     if (answer.isCorrect && answer.time > 20) {
-      score -= 50;
+      score -= CORRECT_SLOW_ANSWER;
     }
   });
-  score += lives * 50;
+  score += lives * LIVE_COST;
 
   return score;
 };
