ff695eac8e6d93579aa048dd7a7c8337c10404f1
Stas Melekhov (devilportez) <devilportez@icloud.com>
2018-04-12 00:06:19 +0500

    Changes

--- js/countScore.js
+++ js/countScore.js
@@ -1,27 +1,30 @@
 const countScore = (answers, lives) => {
   const CORRECT_ANSWER = 100;
-  const CORRECT_FAST_ANSWER = 150;
-  const CORRECT_SLOW_ANSWER = 50;
-  const LIVE_COST = 50;
-  let score = 0;
+  const EXTRA_SCORE = 50;
 
   if (answers.length < 10) {
     return -1;
   }
-  answers.reduce((answer) => {
+
+  const answerScore = answers.reduce((score, answer) => {
     if (answer.isCorrect) {
-      score += CORRECT_ANSWER;
-      if (answer.isCorrect && answer.time < 10) {
-        score += CORRECT_FAST_ANSWER;
+      if (answer.time <= 10) {
+        return score + (CORRECT_ANSWER + EXTRA_SCORE);
       }
-      if (answer.isCorrect && answer.time > 20) {
-        score -= CORRECT_SLOW_ANSWER;
+      if (answer.time <= 20) {
+        return score + CORRECT_ANSWER;
+      }
+      if (answer.time <= 30) {
+        return score + (CORRECT_ANSWER - EXTRA_SCORE);
       }
     }
-  });
-  score += lives * LIVE_COST;
 
-  return score;
+    return score;
+  }, 0);
+
+  const livesScore = lives * EXTRA_SCORE;
+
+  return answerScore + livesScore;
 };
 
 export default countScore;
--- js/createTimer.js
+++ js/createTimer.js
@@ -1,12 +1,14 @@
 const createTimer = (time) => ({
   'time': time,
   tick() {
+    if (this.time > 0) {
+      this.time--;
+    }
     if (this.time === 0) {
       return `Your time is up`;
     }
-    this.time--;
 
-    return this.time || `Your time is up`;
+    return this.time;
   }
 });
 
